50af979208cc0c1475e3adb6306831230ce24e04
Kevin Brown <kevin@webstackbuilders.com>
2026-04-24 04:07:42 +0300

    Add favicon

--- newsletter_maker/urls.py
+++ newsletter_maker/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.shortcuts import redirect
 from django.urls import include, path
-
+from django.views.generic.base import RedirectView
 
 def root_redirect_view(request):
     return redirect("/admin/")
@@ -9,6 +9,7 @@ def root_redirect_view(request):
 urlpatterns = [
     path("", root_redirect_view),
     path("admin/", admin.site.urls),
+    path("favicon.ico", RedirectView.as_view(url="/static/core/favicon.ico", permanent=True)),
     path("api/v1/", include(("core.api_urls", "api"), namespace="v1")),
     path("", include("core.urls")),
 ]
